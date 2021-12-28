e78ee282873108ac953f0a53535a84b93d7219be
hamzampbstars <89343438+hamzampbstars@users.noreply.github.com>
2021-12-28 19:05:06 +0300

    Project Open source code

---
+++ qrcode/qrcode/Form1.Designer.vb
@@ -0,0 +1,127 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form, bileşen listesini temizlemeyi bırakmayı geçersiz kılar.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form Tasarımcısı tarafından gerektirilir
+    Private components As System.ComponentModel.IContainer
+
+    'NOT: Aşağıdaki yordam Windows Form Tasarımcısı için gereklidir
+    'Windows Form Tasarımcısı kullanılarak değiştirilebilir.  
+    'Kod düzenleyicisini kullanarak değiştirmeyin.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.pc1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.pc1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pc1
+        '
+        Me.pc1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.pc1.BackColor = System.Drawing.Color.FromArgb(CType(CType(66, Byte), Integer), CType(CType(66, Byte), Integer), CType(CType(66, Byte), Integer))
+        Me.pc1.Location = New System.Drawing.Point(63, 32)
+        Me.pc1.Name = "pc1"
+        Me.pc1.Size = New System.Drawing.Size(350, 350)
+        Me.pc1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.pc1.TabIndex = 0
+        Me.pc1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(1, Byte), Integer), CType(CType(1, Byte), Integer), CType(CType(1, Byte), Integer))
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Cross
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(63, 501)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(346, 42)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Create QR"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button2.BackColor = System.Drawing.Color.FromArgb(CType(CType(1, Byte), Integer), CType(CType(1, Byte), Integer), CType(CType(1, Byte), Integer))
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.Cross
+        Me.Button2.ForeColor = System.Drawing.Color.White
+        Me.Button2.Location = New System.Drawing.Point(63, 562)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(346, 42)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Save QR : PNG"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(55, Byte), Integer), CType(CType(55, Byte), Integer), CType(CType(55, Byte), Integer))
+        Me.TextBox1.ForeColor = System.Drawing.Color.White
+        Me.TextBox1.Location = New System.Drawing.Point(63, 429)
+        Me.TextBox1.MaxLength = 1000
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(346, 36)
+        Me.TextBox1.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Myanmar Text", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(203, 294)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(70, 36)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "-Link-"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(33, Byte), Integer), CType(CType(33, Byte), Integer), CType(CType(33, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(477, 637)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.pc1)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Windows QR Code"
+        CType(Me.pc1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents pc1 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ qrcode/qrcode/Form1.vb
@@ -0,0 +1,31 @@
+﻿Imports QRCoder
+Imports qrcode
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim olustur As New QRCodeGenerator
+        Dim qrcodenew = olustur.CreateQrCode(TextBox1.Text, QRCodeGenerator.ECCLevel.Q)
+        Dim code As New QRCoder.QRCode(qrcodenew)
+        pc1.Image = code.GetGraphic(20)
+
+
+
+
+
+
+
+
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim yolSec As New SaveFileDialog
+        yolSec.Filter = "QR Form's (.QR) | *.png"
+        yolSec.Title = "QR Code Maker"
+        yolSec.FileName = "QR-Code-Maker"
+        If yolSec.ShowDialog = DialogResult.OK Then
+            pc1.Image.Save(yolSec.FileName)
+        End If
+    End Sub
+End Class
